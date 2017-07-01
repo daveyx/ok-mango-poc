962be17a0dba9a86fe5d97174a570c519022511a
daveyx <d.rehle@gmail.com>
2017-07-01 08:34:20 +0200

    test createuser

--- src/main/java/ok/mango/poc/mangopay/MangopayService.java
+++ src/main/java/ok/mango/poc/mangopay/MangopayService.java
@@ -1,10 +1,16 @@
 package ok.mango.poc.mangopay;
 
+import org.joda.time.DateTime;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import com.mangopay.MangoPayApi;
 import com.mangopay.core.OAuthToken;
+import com.mangopay.core.enumerations.CountryIso;
+import com.mangopay.entities.User;
+import com.mangopay.entities.UserNatural;
 
 /**
  *
@@ -17,35 +23,61 @@ public class MangopayService implements IMangopayService {
 
 	@Value("${ok.mango.api}")
 	private String mangoApi;
-	
+
 	@Value("${ok.mango.clientid}")
 	private String mangoClientId;
 
 	@Value("${ok.mango.clientpw}")
 	private String mangoClientPw;
 
-	public void authMangoPay() {
+	private final MangoPayApi api;
 
-		final MangoPayApi api = new MangoPayApi();
+	private String authToken;
 
-		// configuration
+	private String tokenType;
+
+	private DateTime expiration;
+
+	public MangopayService() {
+		api = new MangoPayApi();
 		api.Config.ClientId = mangoClientId;
 		api.Config.ClientPassword = mangoClientPw;
 		api.Config.BaseUrl = mangoApi;
+		authToken = null;
+		tokenType = null;
+		expiration = new DateTime();
+	}
 
-		// call some API methods...
+	public void authMangoPay() {
 		try {
-//			List<User> users = api.Users.getAll();
+			// List<User> users = api.Users.getAll();
 			final OAuthToken token = api.AuthenticationManager.createToken();
-			System.out.println("token=" + token.access_token + ", type=" + token.token_type + ", expires=" + token.expires_in);
+			System.out.println(
+					"token=" + token.access_token + ", type=" + token.token_type + ", expires=" + token.expires_in);
+		} catch (final Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public User createUserDaveyx() {
+		final DateTimeFormatter formatter = DateTimeFormat.forPattern("dd/MM/yyyy");
+		final DateTime birthday = formatter.parseDateTime("09/09/1980");
+
+		final UserNatural user = new UserNatural();
+		user.setFirstName("David");
+		user.setLastName("Rehle");
+		user.setEmail("d.rehle@gmail.com");
+		user.setBirthday(birthday.getMillis() / 1000);
+		user.setNationality(CountryIso.DE);
+		user.setCountryOfResidence(CountryIso.DE);
+
+		try {
+			api.getUserApi().create(user);
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-	}
-	
-	public void getUser() {
-		
+		return null;
 	}
 
 }
