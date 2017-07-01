f47af812d2b168f11f2a987d319c597561f1a32b
daveyx <d.rehle@gmail.com>
2017-07-01 15:52:55 +0200

    findbyemail

--- src/main/java/ok/mango/poc/persistence/UserRepository.java
+++ src/main/java/ok/mango/poc/persistence/UserRepository.java
@@ -11,5 +11,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface UserRepository extends JpaRepository<PUser, Long> {
 
     PUser findByUsername(final String username);
-
+    PUser findByEmail(final String email);
 }
