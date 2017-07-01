bcf7db303a67fd89192220036ccde432d4730a00
daveyx <d.rehle@gmail.com>
2017-07-01 10:39:24 +0200

    code format

--- src/main/java/ok/mango/poc/security/FacebookSignInAdapter.java
+++ src/main/java/ok/mango/poc/security/FacebookSignInAdapter.java
@@ -18,11 +18,12 @@ import org.springframework.web.context.request.NativeWebRequest;
 
 @Service
 public class FacebookSignInAdapter implements SignInAdapter {
-	
-    @Override
-    public String signIn(String localUserId, Connection<?> connection, NativeWebRequest request) {
-        System.out.println(" ====== Sign In adapter");
-        SecurityContextHolder.getContext().setAuthentication(new UsernamePasswordAuthenticationToken(connection.getDisplayName(), null, Arrays.asList(new SimpleGrantedAuthority("FACEBOOK_USER"))));
-        return null;
-    }
+
+	@Override
+	public String signIn(final String localUserId, final Connection<?> connection, final NativeWebRequest request) {
+		System.out.println(" ====== Sign In adapter");
+		SecurityContextHolder.getContext().setAuthentication(new UsernamePasswordAuthenticationToken(
+				connection.getDisplayName(), null, Arrays.asList(new SimpleGrantedAuthority("FACEBOOK_USER"))));
+		return null;
+	}
 }
