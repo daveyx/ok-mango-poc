1e7bef49ef6751bd405c11c35e990e2b07e191b8
daveyx <d.rehle@gmail.com>
2017-07-02 15:58:53 +0200

    info about all fields

--- src/main/java/ok/mango/poc/security/FacebookConnectionSignup.java
+++ src/main/java/ok/mango/poc/security/FacebookConnectionSignup.java
@@ -29,6 +29,21 @@ public class FacebookConnectionSignup implements ConnectionSignUp {
 		System.out.println("signup === ");
 
 		final Facebook facebook = (Facebook) connection.getApi();
+		// { "id", "about", "age_range", "birthday", "context", "cover",
+		// "currency", "devices", "education", "email", "favorite_athletes",
+		// "favorite_teams", "first_name", "gender", "hometown",
+		// "inspirational_people",
+		// "installed", "install_type", "is_verified", "languages", "last_name",
+		// "link",
+		// "locale", "location", "meeting_for", "middle_name", "name",
+		// "name_format",
+		// "political", "quotes", "payment_pricepoints", "relationship_status",
+		// "religion",
+		// "security_settings", "significant_other", "sports", "test_group",
+		// "timezone",
+		// "third_party_id", "updated_time", "verified", "video_upload_limits",
+		// "viewer_can_send_gift", "website", "work"}
+
 		final String[] fields = { "id", "email", "first_name", "last_name" };
 		final User userProfile = facebook.fetchObject("me", User.class, fields);
 
