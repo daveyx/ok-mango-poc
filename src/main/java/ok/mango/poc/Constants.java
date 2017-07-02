2682762d7fd271432f6d0627bbbb78f8b956e1d8
daveyx <d.rehle@gmail.com>
2017-07-02 16:02:07 +0200

    output some info about a facebookuser

---
+++ src/main/java/ok/mango/poc/Constants.java
@@ -0,0 +1,8 @@
+package ok.mango.poc;
+
+public class Constants {
+	
+	public static final String ROLE_USER = "ROLE_USER";
+	public static final String ROLE_FACEBOOK_USER = "ROLE_FACEBOOK_USER";
+
+}
--- src/main/java/ok/mango/poc/controllers/PocController.java
+++ src/main/java/ok/mango/poc/controllers/PocController.java
@@ -1,7 +1,12 @@
 package ok.mango.poc.controllers;
 
+import java.util.Collection;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.social.facebook.api.Facebook;
+import org.springframework.social.facebook.api.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,8 +14,8 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
+import ok.mango.poc.Constants;
 import ok.mango.poc.mangopay.IMangopayService;
-import ok.mango.poc.mangopay.MangopayService;
 
 /**
  *
@@ -20,27 +25,49 @@ import ok.mango.poc.mangopay.MangopayService;
 
 @Controller
 public class PocController {
-	
+
 	@Autowired
 	private IMangopayService mpService;
-	
-    @RequestMapping(method=RequestMethod.GET, value="/")
-    public String poc(@RequestParam(value="name", required=false, defaultValue="World") String name, final Model model) {
-        return "index";
-    }
-    
-    @RequestMapping(method=RequestMethod.POST, value="execute")
-    public ModelAndView execute(@RequestParam(value="action", required=false) final String action, final Model model) {
-    	System.out.println("action=" + action);
-    	final ModelAndView mav = new ModelAndView("index"); 
-    	mav.addObject("message", "peter");
-//    	mpService.authMangoPay();
-    	mpService.createWalletDaveyx();
-    	return mav;
-    }
-    
-//    @Bean
-//    public IMangopayService getMangepayService() {
-//    	return new MangopayService();
-//    }
+
+	@Autowired
+	private Facebook facebook;
+
+	@RequestMapping(method = RequestMethod.GET, value = "/")
+	public ModelAndView poc(@RequestParam(value = "name", required = false, defaultValue = "World") String name,
+			final Model model) {
+		final Collection<? extends GrantedAuthority> auth = 
+				SecurityContextHolder.getContext().getAuthentication().getAuthorities();
+		boolean isFacebookUser = false;
+		for (final GrantedAuthority ga : auth) {
+			if (ga.getAuthority().equals(Constants.ROLE_FACEBOOK_USER)) {
+				isFacebookUser = true;
+				break;
+			}
+		}
+		final ModelAndView mav = new ModelAndView("index");
+		if (isFacebookUser) {
+			final String[] fields = { "id", "email", "first_name", "last_name", "cover" };
+			final User userProfile = facebook.fetchObject("me", User.class, fields);
+			mav.addObject("fbuser", true);
+			mav.addObject("id", userProfile.getId());
+			mav.addObject("email", userProfile.getEmail());
+			mav.addObject("name", userProfile.getFirstName() + " " + userProfile.getLastName());
+			mav.addObject("img", "https://graph.facebook.com/" + userProfile.getId() + "/picture");
+		} else {
+			mav.addObject("fbuser", false);
+		}
+		return mav;
+	}
+
+	@RequestMapping(method = RequestMethod.POST, value = "execute")
+	public ModelAndView execute(@RequestParam(value = "action", required = false) final String action,
+			final Model model) {
+		System.out.println("action=" + action);
+		final ModelAndView mav = new ModelAndView("index");
+		mav.addObject("message", "peter");
+		// mpService.authMangoPay();
+		mpService.createWalletDaveyx();
+		return mav;
+	}
+
 }
