d54b3d18e9f6fd03fcfbd960e25c42ba3a4de697
daveyx <d.rehle@gmail.com>
2017-07-03 07:05:45 +0200

    userdetails as principal

--- src/main/java/ok/mango/poc/security/FacebookSignInAdapter.java
+++ src/main/java/ok/mango/poc/security/FacebookSignInAdapter.java
@@ -5,6 +5,7 @@ import java.util.Arrays;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.social.connect.Connection;
 import org.springframework.social.connect.web.SignInAdapter;
 import org.springframework.stereotype.Service;
@@ -24,8 +25,16 @@ public class FacebookSignInAdapter implements SignInAdapter {
 	@Override
 	public String signIn(final String localUserId, final Connection<?> connection, final NativeWebRequest request) {
 		System.out.println(" ====== Sign In adapter");
-		SecurityContextHolder.getContext().setAuthentication(new UsernamePasswordAuthenticationToken(
-				localUserId, null, Arrays.asList(new SimpleGrantedAuthority(Constants.ROLE_FACEBOOK_USER))));
+
+		final User user = new User(localUserId, "null", true, true, true, true,
+				Arrays.asList(new SimpleGrantedAuthority(Constants.ROLE_FACEBOOK_USER)));
+
+		// SecurityContextHolder.getContext().setAuthentication(new
+		// UsernamePasswordAuthenticationToken(
+		// user, null, Arrays.asList(new
+		// SimpleGrantedAuthority(Constants.ROLE_FACEBOOK_USER))));
+		SecurityContextHolder.getContext().setAuthentication(new UsernamePasswordAuthenticationToken(user, null,
+				Arrays.asList(new SimpleGrantedAuthority(Constants.ROLE_FACEBOOK_USER))));
 		return null;
 	}
 }
