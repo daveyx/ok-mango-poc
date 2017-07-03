fb10f4899af729d2ee51db66a4b16fe0febd0e6a
daveyx <d.rehle@gmail.com>
2017-07-03 07:14:14 +0200

    added mango info

--- src/main/java/ok/mango/poc/controllers/MangoController.java
+++ src/main/java/ok/mango/poc/controllers/MangoController.java
@@ -5,11 +5,14 @@ import java.io.IOException;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+
+import com.mangopay.entities.User;
 
 import ok.mango.poc.mangopay.IMangopayService;
 
@@ -41,9 +44,16 @@ public class MangoController {
 	}
 
 	@RequestMapping(method = RequestMethod.GET, value = "mango/payment")
-	public String mangoPayment() {
+	public ModelAndView mangoPayment() {
+		final String email = ((org.springframework.security.core.userdetails.User) SecurityContextHolder.getContext()
+				.getAuthentication().getPrincipal()).getUsername();
+		final User mangoUser = mpService.getUserByEmail(email);
+
+		final ModelAndView mav = new ModelAndView("mango/payment");
+		mav.addObject("mangoinfo",
+				mangoUser == null ? "you are not yet a mango user" : "your mango user id is " + mangoUser.getId());
 		System.out.println("ok.mango.poc.controllers.MangoController.mangoTemplate()");
-		return "mango/payment";
+		return mav;
 	}
 
 	@RequestMapping(method = RequestMethod.POST, value = "execute")
